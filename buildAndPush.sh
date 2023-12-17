9e80daaf268fe319b5220d5f04a2818ce8677459
ds <fuzehua@syoo.cn>
2023-12-18 07:08:50 +0800

    update

---
+++ buildAndPush.sh
@@ -0,0 +1,3 @@
+GOOS=linux GOARCH=amd64 go build -o temp/linux_amd64/main main.go
+docker build -f manifest/docker/Dockerfile -t registry.cn-shenzhen.aliyuncs.com/heiku/heiku_gooverseapay:daily .
+docker push registry.cn-shenzhen.aliyuncs.com/heiku/heiku_gooverseapay:daily
