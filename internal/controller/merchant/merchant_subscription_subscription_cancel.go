c2df30a759cc0bf2cf63fb9cb6dc9dee57521890
ds <fuzehua@syoo.cn>
2024-01-08 10:00:47 +0800

    sub cancel

---
+++ internal/controller/merchant/merchant_subscription_subscription_cancel.go
@@ -0,0 +1,14 @@
+package merchant
+
+import (
+	"context"
+
+	"github.com/gogf/gf/v2/errors/gcode"
+	"github.com/gogf/gf/v2/errors/gerror"
+
+	"go-oversea-pay/api/merchant/subscription"
+)
+
+func (c *ControllerSubscription) SubscriptionCancel(ctx context.Context, req *subscription.SubscriptionCancelReq) (res *subscription.SubscriptionCancelRes, err error) {
+	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+}
