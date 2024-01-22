aa2902544ac4d8637e9c65470421d87f079fe7db
ds <fuzehua@syoo.cn>
2024-01-22 16:37:48 +0800

    payment

--- internal/logic/subscription/service/subscription.go
+++ internal/logic/subscription/service/subscription.go
@@ -477,8 +477,8 @@ func SubscriptionUpdatePreview(ctx context.Context, req *subscription.Subscripti
 		}
 	}
 
-	if sub.TrialEnd > gtime.Now().Timestamp() {
-		//试用期内，所有变更马上生效 todo mark 讨论
+	if merchantUserId > 0 && sub.TrialEnd > gtime.Now().Timestamp() {
+		//试用期内，Merchant Portal 所有变更马上生效 todo mark 讨论
 		effectImmediate = true
 	}
 
