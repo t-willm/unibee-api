1c5ff2b22cd4586aac1f7805d8be6122b579bd5a
ds <fuzehua@syoo.cn>
2024-01-22 15:48:05 +0800

    payment

--- internal/controller/merchant/merchant_subscription_subscription_update_preview.go
+++ internal/controller/merchant/merchant_subscription_subscription_update_preview.go
@@ -13,10 +13,12 @@ import (
 
 func (c *ControllerSubscription) SubscriptionUpdatePreview(ctx context.Context, req *subscription.SubscriptionUpdatePreviewReq) (res *subscription.SubscriptionUpdatePreviewRes, err error) {
 	//Update 可以由 Admin 操作，service 层不做用户校验
+	var merchantUserId int64
 	if !consts.GetConfigInstance().IsLocal() {
 		//User 检查
 		utility.Assert(_interface.BizCtx().Get(ctx).MerchantUser != nil, "merchant auth failure,not login")
 		utility.Assert(_interface.BizCtx().Get(ctx).MerchantUser.Id > 0, "merchantUserId invalid")
+		merchantUserId = int64(_interface.BizCtx().Get(ctx).MerchantUser.Id)
 	}
 	update, err := service.SubscriptionUpdatePreview(ctx, &subscription2.SubscriptionUpdatePreviewReq{
 		SubscriptionId:      req.SubscriptionId,
@@ -24,7 +26,7 @@ func (c *ControllerSubscription) SubscriptionUpdatePreview(ctx context.Context, 
 		Quantity:            req.Quantity,
 		AddonParams:         req.AddonParams,
 		WithImmediateEffect: req.WithImmediateEffect,
-	}, 0, int64(_interface.BizCtx().Get(ctx).MerchantUser.Id))
+	}, 0, merchantUserId)
 	if err != nil {
 		return nil, err
 	}
