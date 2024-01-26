9a83cac22cd67c2ed2f239581a4d5234baf760bc
ds <fuzehua@syoo.cn>
2024-01-26 10:48:10 +0800

    payment

---
+++ internal/cronjob/channel_log/delete.go
@@ -0,0 +1,64 @@
+package channel_log
+
+// https://goframe.org/pages/viewpage.action?pageId=30736417
+func deleteChannelLogs() error {
+	//var (
+	//	err error
+	//	ctx = gctx.New()
+	//)
+	//_, err = gcron.Add(ctx, "* * * * * *", func(ctx context.Context) {
+	//	g.Log().Print(ctx, "Every second")
+	//}, "MySecondCronJob")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//_, err = gcron.Add(ctx, "0 30 * * * *", func(ctx context.Context) {
+	//	g.Log().Print(ctx, "Every hour on the half hour")
+	//})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//_, err = gcron.Add(ctx, "@hourly", func(ctx context.Context) {
+	//	g.Log().Print(ctx, "Every hour")
+	//})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//_, err = gcron.Add(ctx, "@every 1h30m", func(ctx context.Context) {
+	//	g.Log().Print(ctx, "Every hour thirty")
+	//})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//g.Dump(gcron.Entries())
+	//
+	//time.Sleep(3 * time.Second)
+	//
+	//g.Log().Print(ctx, `stop cronjob "MySecondCronJob"`)
+	//gcron.Stop("MySecondCronJob")
+	//
+	//time.Sleep(3 * time.Second)
+	//
+	//g.Log().Print(ctx, `start cronjob "MySecondCronJob"`)
+	//gcron.Start("MySecondCronJob")
+	//
+	//time.Sleep(3 * time.Second)
+
+	//var (
+	//	err error
+	//	ctx = gctx.New()
+	//)
+	//_, err = gcron.AddSingleton(ctx, "* * * * * *", func(ctx context.Context) {
+	//	g.Log().Print(ctx, "doing")
+	//	time.Sleep(2 * time.Second)
+	//})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//select {}
+	return nil
+}
---
+++ internal/cronjob/cronjob.go
@@ -0,0 +1,5 @@
+package cronjob
+
+func StartCronJobs() error {
+	return nil
+}
