b10a5856a006ef7898e5276b50bb2b3bd7ed65bd
ds <fuzehua@syoo.cn>
2024-02-01 18:24:59 +0800

    payment

--- internal/cronjob/cronjob.go
+++ internal/cronjob/cronjob.go
@@ -16,6 +16,7 @@ func StartCronJobs() {
 	//if consts.GetConfigInstance().IsServerDev() {
 	//	return
 	//}
+	// every 10 second
 	var name = "SubscriptionBillingCycleDunningInvoice"
 	g.Log().Print(ctx, "CronJob Start......")
 	_, err = gcron.AddSingleton(ctx, "*/10 * * * * *", func(ctx context.Context) {
@@ -24,12 +25,21 @@ func StartCronJobs() {
 	if err != nil {
 		g.Log().Print(ctx, "StartCronJobs Name:%s Err:%s", name, err.Error())
 	}
+	// every 10 second
 	var backName = "SubscriptionBillingCycleDunningInvoiceBackup"
 	_, err = gcron.AddSingleton(ctx, "*/10 * * * * *", func(ctx context.Context) {
 		sub.SubscriptionBillingCycleDunningInvoice(ctx, backName)
 	}, backName)
 	if err != nil {
 		g.Log().Print(ctx, "StartCronJobs Name:%s Err:%s", backName, err.Error())
+	}
+	// every hour
+	var httpLogDeleteTaskName = "httpLogDeleteTaskName"
+	_, err = gcron.AddSingleton(ctx, "* * */1 * * *", func(ctx context.Context) {
+
+	}, backName)
+	if err != nil {
+		g.Log().Print(ctx, "StartCronJobs Name:%s Err:%s", httpLogDeleteTaskName, err.Error())
 	}
 	return
 }
