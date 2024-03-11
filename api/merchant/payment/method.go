070f073b67e815fe186612b139f6f875032cd46f
jack <redcatmiss@126.com>
2024-03-11 23:40:02 +0800

    changelly development

--- api/merchant/payment/method.go
+++ api/merchant/payment/method.go
@@ -6,7 +6,7 @@ import (
 )
 
 type MethodListReq struct {
-	g.Meta    `path:"/method_list" tags:"User-Payment-Method" method:"get" summary:"Query User Payment Method List"`
+	g.Meta    `path:"/method_list" tags:"Payment" method:"get" summary:"Query Payment Method List"`
 	GatewayId uint64 `json:"gatewayId" dc:"GatewayId"   v:"required" `
 	UserId    uint64 `json:"userId" dc:"UserId"   v:"required" `
 	PaymentId string `json:"paymentId" dc:"PaymentId"  `
