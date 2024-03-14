de48c706b7410c0d1871cb67deeea94540de8d66
jack <redcatmiss@126.com>
2024-03-14 14:13:01 +0800

    bean refactor

--- internal/logic/crypto/crypto.go
+++ internal/logic/crypto/crypto.go
@@ -1,9 +1,14 @@
 package crypto
 
+import "unibee/internal/consts"
+
 func GetCryptoCurrency() string {
 	return "USDT"
 }
 
 func GetCryptoAmount(totalAmount int64, taxAmount int64) int64 {
-	return totalAmount + taxAmount // todo mark 1:1 now
+	if consts.GetConfigInstance().IsLocal() || consts.GetConfigInstance().IsStage() {
+		return totalAmount / 100
+	}
+	return totalAmount // todo mark 1:1 now
 }
