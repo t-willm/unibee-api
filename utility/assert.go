88f1defab3da2d4ba5dba079ed8023cdbc0f2801
jack <redcatmiss@126.com>
2024-03-22 17:16:14 +0800

    pgsql support

--- utility/assert.go
+++ utility/assert.go
@@ -19,7 +19,6 @@ func AssertError(err error, message string) {
 	}
 }
 
-// Try Cache 模拟，捕获内部异常，实际上框架已实现恢复机制
 func Try(fun func(), handler func(interface{})) {
 	defer func() {
 		if err := recover(); err != nil {
