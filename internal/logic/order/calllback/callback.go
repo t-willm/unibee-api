af2a10ed7ee54837f8e7e57216614bb4875f6f54
jack <redcatmiss@126.com>
2024-03-22 15:16:31 +0800

    pgsql support

--- internal/logic/order/calllback/callback.go
+++ internal/logic/order/calllback/callback.go
@@ -10,50 +10,49 @@ type MerchantOneTimePaymentCallback struct {
 
 func (m MerchantOneTimePaymentCallback) PaymentRefundCancelCallback(ctx context.Context, payment *entity.Payment, refund *entity.Refund) {
 	//TODO implement me
-	panic("implement me")
 }
 
 func (m MerchantOneTimePaymentCallback) PaymentRefundCreateCallback(ctx context.Context, payment *entity.Payment, refund *entity.Refund) {
 	//TODO implement me
-	panic("implement me")
+
 }
 
 func (m MerchantOneTimePaymentCallback) PaymentRefundSuccessCallback(ctx context.Context, payment *entity.Payment, refund *entity.Refund) {
 	//TODO implement me
-	panic("implement me")
+
 }
 
 func (m MerchantOneTimePaymentCallback) PaymentRefundFailureCallback(ctx context.Context, payment *entity.Payment, refund *entity.Refund) {
 	//TODO implement me
-	panic("implement me")
+
 }
 
 func (m MerchantOneTimePaymentCallback) PaymentRefundReverseCallback(ctx context.Context, payment *entity.Payment, refund *entity.Refund) {
 	//TODO implement me
-	panic("implement me")
+
 }
 
 func (m MerchantOneTimePaymentCallback) PaymentNeedAuthorisedCallback(ctx context.Context, payment *entity.Payment, invoice *entity.Invoice) {
 	//TODO implement me
-	panic("implement me")
+
 }
 
 func (m MerchantOneTimePaymentCallback) PaymentCancelCallback(ctx context.Context, payment *entity.Payment, invoice *entity.Invoice) {
 	//TODO implement me
-	panic("implement me")
+
 }
 
 func (m MerchantOneTimePaymentCallback) PaymentCreateCallback(ctx context.Context, payment *entity.Payment, invoice *entity.Invoice) {
 	//TODO implement me
-	panic("implement me")
+
 }
 
 func (m MerchantOneTimePaymentCallback) PaymentSuccessCallback(ctx context.Context, payment *entity.Payment, invoice *entity.Invoice) {
 	//TODO implement me
-	panic("implement me")
+
 }
 
 func (m MerchantOneTimePaymentCallback) PaymentFailureCallback(ctx context.Context, payment *entity.Payment, invoice *entity.Invoice) {
 	//TODO implement me
-	panic("implement me")
+
 }
