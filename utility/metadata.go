1495c631e0ec94e0f0de3bf41650101a223e0499
jack <redcatmiss@126.com>
2024-04-19 21:34:40 +0800

    metadata type change

--- utility/metadata.go
+++ utility/metadata.go
@@ -3,7 +3,7 @@ package utility
 import "fmt"
 
 func ConvertToStringMetadata(metadata map[string]interface{}) map[string]string {
-	var convertMap map[string]string
+	var convertMap = make(map[string]string)
 	for key, value := range metadata {
 		convertMap[key] = fmt.Sprintf("%v", value)
 	}
