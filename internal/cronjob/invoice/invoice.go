78aea68a4197c83071bfdc4fec87063bf7235ca9
jack <redcatmiss@126.com>
2024-04-27 16:52:01 +0800

    Compensate discount to invoice lines

--- internal/cronjob/invoice/invoice.go
+++ internal/cronjob/invoice/invoice.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gtime"
 	"unibee/internal/consts"
 	dao "unibee/internal/dao/oversea_pay"
+	"unibee/internal/logic/invoice/service"
 	entity "unibee/internal/model/entity/oversea_pay"
 	"unibee/utility"
 )
@@ -15,6 +17,7 @@ func TaskForExpireInvoices(ctx context.Context) {
 	err := dao.Invoice.Ctx(ctx).
 		Where(dao.Invoice.Columns().Status, consts.InvoiceStatusProcessing).
 		Where(dao.Invoice.Columns().IsDeleted, 0).
+		OrderAsc(dao.Invoice.Columns().FinishTime).
 		Scan(&list)
 	if err != nil {
 		g.Log().Errorf(ctx, "TaskForExpireInvoices error:%s", err.Error())
@@ -28,9 +31,23 @@ func TaskForExpireInvoices(ctx context.Context) {
 				utility.ReleaseLock(ctx, key)
 				g.Log().Print(ctx, "TaskForExpireInvoices ReleaseLock", key)
 			}()
-			// todo mark expire invoice
+			if one.FinishTime == 0 {
+				_, err = dao.Invoice.Ctx(ctx).Data(g.Map{
+					dao.Invoice.Columns().FinishTime: one.GmtModify.Timestamp(),
+					dao.Invoice.Columns().GmtModify:  gtime.Now(),
+				}).Where(dao.Invoice.Columns().Id, one.Id).OmitNil().Update()
+				if err != nil {
+					g.Log().Errorf(ctx, "TaskForExpireInvoices update finish error:", err.Error())
+				}
+			} else if one.FinishTime+(one.DayUtilDue*86400) < gtime.Now().Timestamp() {
+				//Invoice Expire
+				err = service.CancelProcessingInvoice(ctx, one.InvoiceId)
+				if err != nil {
+					g.Log().Errorf(ctx, "TaskForExpireInvoices cancel invoice error:", err.Error())
+				}
+			}
 		} else {
-			g.Log().Print(ctx, "TaskForExpireInvoices GetLock Failure", key)
+			g.Log().Errorf(ctx, "TaskForExpireInvoices GetLock Failure", key)
 			return
 		}
 	}
