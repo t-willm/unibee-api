a5d70bab1ffd6a59910a021bd417f914b7b050e6
jack <redcatmiss@126.com>
2024-05-01 02:49:00 +0800

    Refund Invoice Pdf

--- internal/controller/merchant/merchant_subscription_subscription_update_cancel_at_period_end.go
+++ internal/controller/merchant/merchant_subscription_subscription_update_cancel_at_period_end.go
@@ -3,12 +3,11 @@ package merchant
 import (
 	"context"
 	"unibee/api/merchant/subscription"
-	_interface "unibee/internal/interface"
 	"unibee/internal/logic/subscription/service"
 )
 
 func (c *ControllerSubscription) CancelAtPeriodEnd(ctx context.Context, req *subscription.CancelAtPeriodEndReq) (res *subscription.CancelAtPeriodEndRes, err error) {
-	err = service.SubscriptionCancelAtPeriodEnd(ctx, req.SubscriptionId, false, int64(_interface.Context().Get(ctx).MerchantMember.Id))
+	err = service.SubscriptionCancelAtPeriodEnd(ctx, req.SubscriptionId, false, -1)
 	if err != nil {
 		return nil, err
 	}
--- internal/controller/merchant/merchant_subscription_subscription_update_preview.go
+++ internal/controller/merchant/merchant_subscription_subscription_update_preview.go
@@ -2,25 +2,11 @@ package merchant
 
 import (
 	"context"
-	"github.com/gogf/gf/v2/frame/g"
-	"unibee/internal/cmd/config"
-	_interface "unibee/internal/interface"
-	"unibee/internal/logic/subscription/service"
-	"unibee/utility"
-
 	"unibee/api/merchant/subscription"
+	"unibee/internal/logic/subscription/service"
 )
 
 func (c *ControllerSubscription) UpdatePreview(ctx context.Context, req *subscription.UpdatePreviewReq) (res *subscription.UpdatePreviewRes, err error) {
-	//Update 可以由 Admin 操作，service 层不做用户校验
-	var merchantMemberId int64
-	if !config.GetConfigInstance().IsLocal() {
-		//User 检查
-		utility.Assert(_interface.Context().Get(ctx).MerchantMember != nil, "merchant auth failure,not login")
-		utility.Assert(_interface.Context().Get(ctx).MerchantMember.Id > 0, "merchantMemberId invalid")
-		merchantMemberId = int64(_interface.Context().Get(ctx).MerchantMember.Id)
-	}
-	g.Log().Infof(ctx, "SubscriptionUpdatePreview merchantMemberId:%d", merchantMemberId)
 	update, err := service.SubscriptionUpdatePreview(ctx, &service.UpdatePreviewInternalReq{
 		SubscriptionId:  req.SubscriptionId,
 		NewPlanId:       req.NewPlanId,
@@ -28,7 +14,7 @@ func (c *ControllerSubscription) UpdatePreview(ctx context.Context, req *subscri
 		AddonParams:     req.AddonParams,
 		EffectImmediate: req.EffectImmediate,
 		DiscountCode:    req.DiscountCode,
-	}, 0, merchantMemberId)
+	}, 0, -1)
 	if err != nil {
 		return nil, err
 	}
