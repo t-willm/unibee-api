d39652375bc82c09a18747616e8f1b8613532e6d
jack <redcatmiss@126.com>
2024-05-09 11:32:22 +0800

    trial invoice email

--- api/merchant/invoice/invoice.go
+++ api/merchant/invoice/invoice.go
@@ -143,7 +143,7 @@ type MarkRefundRes struct {
 type MarkWireTransferSuccessReq struct {
 	g.Meta         `path:"/mark_wire_transfer_success" tags:"Invoice" method:"post" summary:"MarkWireTransferInvoiceSuccess" dc:"Mark wire transfer pending invoice as success"`
 	InvoiceId      string `json:"invoiceId" dc:"The unique id of invoice" v:"required"`
-	TransferNumber string `json:"reason" dc:"The transfer number of invoice" v:"required"`
+	TransferNumber string `json:"transferNumber" dc:"The transfer number of invoice" v:"required"`
 }
 
 type MarkWireTransferSuccessRes struct {
