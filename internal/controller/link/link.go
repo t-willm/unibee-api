b70d0db1feb243acd59e00b92c26f755a4f9b4e0
jack <redcatmiss@126.com>
2024-05-14 01:37:37 +0800

    upgrade to different interval plan

--- internal/controller/link/link.go
+++ internal/controller/link/link.go
@@ -2,6 +2,7 @@ package link
 
 import (
 	"fmt"
+	"github.com/gogf/gf/v2/os/gtime"
 	"unibee/internal/cmd/config"
 )
 
@@ -9,14 +10,14 @@ func GetInvoiceLink(invoiceId string, st string) string {
 	if len(invoiceId) == 0 {
 		return ""
 	}
-	return fmt.Sprintf("%s/in/%s?st=%s", config.GetConfigInstance().Server.GetServerPath(), invoiceId, st)
+	return fmt.Sprintf("%s/in/%s?st=%s&t=%d", config.GetConfigInstance().Server.GetServerPath(), invoiceId, st, gtime.Now().Timestamp())
 }
 
 func GetInvoicePdfLink(invoiceId string, st string) string {
 	if len(invoiceId) == 0 {
 		return ""
 	}
-	return fmt.Sprintf("%s/in/pdf/%s?st=%s", config.GetConfigInstance().Server.GetServerPath(), invoiceId, st)
+	return fmt.Sprintf("%s/in/pdf/%s?st=%s&t=%d", config.GetConfigInstance().Server.GetServerPath(), invoiceId, st, gtime.Now().Timestamp())
 }
 
 func GetPaymentLink(paymentId string) string {
