2446759b4c77774816b184a5888d27f0a8bb0d5e
jack <redcatmiss@126.com>
2024-05-13 15:57:16 +0800

    upgrade to different interval plan

--- api/bean/detail/payment_timeline_detail.go
+++ api/bean/detail/payment_timeline_detail.go
@@ -6,53 +6,61 @@ import (
 	"unibee/internal/consts"
 	entity "unibee/internal/model/entity/oversea_pay"
 	"unibee/internal/query"
+	"unibee/utility"
 )
 
 type PaymentTimelineDetail struct {
-	Id             uint64                `json:"id"             description:""`                                //
-	MerchantId     uint64                `json:"merchantId"     description:"merchant id"`                     // merchant id
-	UserId         uint64                `json:"userId"         description:"userId"`                          // userId
-	SubscriptionId string                `json:"subscriptionId" description:"subscription id"`                 // subscription id
-	InvoiceId      string                `json:"invoiceId"      description:"invoice id"`                      // invoice id
-	Currency       string                `json:"currency"       description:"currency"`                        // currency
-	TotalAmount    int64                 `json:"totalAmount"    description:"total amount"`                    // total amount
-	GatewayId      uint64                `json:"gatewayId"      description:"gateway id"`                      // gateway id
-	TransactionId  string                `json:"transactionId"      description:"TransactionId"`               // TransactionId
-	PaymentId      string                `json:"paymentId"      description:"PaymentId"`                       // PaymentId
-	Status         int                   `json:"status"         description:"0-pending, 1-success, 2-failure"` // 0-pending, 1-success, 2-failure
-	TimelineType   int                   `json:"timelineType"   description:"0-pay, 1-refund"`                 // 0-pay, 1-refund
-	CreateTime     int64                 `json:"createTime"     description:"create utc time"`                 // create utc time
-	RefundId       string                `json:"refundId"       description:"refund id"`                       // refund id
-	FullRefund     int                   `json:"fullRefund"     description:"0-no, 1-yes"`                     // 0-no, 1-yes
-	Payment        *bean.PaymentSimplify `json:"payment" dc:"Payment"`
-	Refund         *bean.RefundSimplify  `json:"refund" dc:"Refund"`
+	Id                    uint64                `json:"id"             description:""`                                //
+	MerchantId            uint64                `json:"merchantId"     description:"merchant id"`                     // merchant id
+	UserId                uint64                `json:"userId"         description:"userId"`                          // userId
+	SubscriptionId        string                `json:"subscriptionId" description:"subscription id"`                 // subscription id
+	InvoiceId             string                `json:"invoiceId"      description:"invoice id"`                      // invoice id
+	Currency              string                `json:"currency"       description:"currency"`                        // currency
+	TotalAmount           int64                 `json:"totalAmount"    description:"total amount"`                    // total amount
+	GatewayId             uint64                `json:"gatewayId"      description:"gateway id"`                      // gateway id
+	TransactionId         string                `json:"transactionId"      description:"TransactionId"`               // TransactionId
+	PaymentId             string                `json:"paymentId"      description:"PaymentId"`                       // PaymentId
+	Status                int                   `json:"status"         description:"0-pending, 1-success, 2-failure"` // 0-pending, 1-success, 2-failure
+	TimelineType          int                   `json:"timelineType"   description:"0-pay, 1-refund"`                 // 0-pay, 1-refund
+	CreateTime            int64                 `json:"createTime"     description:"create utc time"`                 // create utc time
+	RefundId              string                `json:"refundId"       description:"refund id"`                       // refund id
+	FullRefund            int                   `json:"fullRefund"     description:"0-no, 1-yes"`                     // 0-no, 1-yes
+	Payment               *bean.PaymentSimplify `json:"payment" dc:"Payment"`
+	Refund                *bean.RefundSimplify  `json:"refund" dc:"Refund"`
+	ExternalTransactionId string                `json:"externalTransactionId"      description:"ExternalTransactionId"` // ExternalTransactionId
 }
 
 func ConvertPaymentTimeline(ctx context.Context, one *entity.PaymentTimeline) *PaymentTimelineDetail {
 	if one == nil {
 		return nil
 	}
+	var payment = bean.SimplifyPayment(query.GetPaymentByPaymentId(ctx, one.PaymentId))
+	utility.Assert(payment != nil, "payment not found:"+one.PaymentId)
+	var refund = bean.SimplifyRefund(query.GetRefundByRefundId(ctx, one.RefundId))
 	var transactionId = one.PaymentId
-	if one.TimelineType == consts.TimelineTypeRefund {
+	var externalTransactionId = payment.GatewayPaymentId
+	if one.TimelineType == consts.TimelineTypeRefund && refund != nil {
 		transactionId = one.RefundId
+		externalTransactionId = refund.GatewayRefundId
 	}
 	return &PaymentTimelineDetail{
-		Id:             one.Id,
-		TransactionId:  transactionId,
-		MerchantId:     one.MerchantId,
-		UserId:         one.UserId,
-		SubscriptionId: one.SubscriptionId,
-		InvoiceId:      one.InvoiceId,
-		Currency:       one.Currency,
-		TotalAmount:    one.TotalAmount,
-		GatewayId:      one.GatewayId,
-		PaymentId:      one.PaymentId,
-		Status:         one.Status,
-		TimelineType:   one.TimelineType,
-		CreateTime:     one.CreateTime,
-		RefundId:       one.RefundId,
-		FullRefund:     one.FullRefund,
-		Payment:        bean.SimplifyPayment(query.GetPaymentByPaymentId(ctx, one.PaymentId)),
-		Refund:         bean.SimplifyRefund(query.GetRefundByRefundId(ctx, one.RefundId)),
+		Id:                    one.Id,
+		TransactionId:         transactionId,
+		ExternalTransactionId: externalTransactionId,
+		MerchantId:            one.MerchantId,
+		UserId:                one.UserId,
+		SubscriptionId:        one.SubscriptionId,
+		InvoiceId:             one.InvoiceId,
+		Currency:              one.Currency,
+		TotalAmount:           one.TotalAmount,
+		GatewayId:             one.GatewayId,
+		PaymentId:             one.PaymentId,
+		Status:                one.Status,
+		TimelineType:          one.TimelineType,
+		CreateTime:            one.CreateTime,
+		RefundId:              one.RefundId,
+		FullRefund:            one.FullRefund,
+		Payment:               payment,
+		Refund:                refund,
 	}
 }
