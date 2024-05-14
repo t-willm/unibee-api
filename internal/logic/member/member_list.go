9d9ccc0210b13f3b272b788cfee98d5d49f541f3
jack <redcatmiss@126.com>
2024-05-14 11:16:04 +0800

    upgrade to different interval plan

--- internal/logic/member/member_list.go
+++ internal/logic/member/member_list.go
@@ -5,17 +5,22 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 	"unibee/api/bean"
 	dao "unibee/internal/dao/oversea_pay"
+	entity "unibee/internal/model/entity/oversea_pay"
 )
 
 func MerchantMemberList(ctx context.Context, merchantId uint64) ([]*bean.MerchantMemberSimplify, int) {
-	var mainList = make([]*bean.MerchantMemberSimplify, 0)
+	var mainList = make([]*entity.MerchantMember, 0)
+	var resultList = make([]*bean.MerchantMemberSimplify, 0)
 	err := dao.MerchantMember.Ctx(ctx).
 		Where(dao.MerchantMember.Columns().MerchantId, merchantId).
 		Where(dao.MerchantMember.Columns().IsDeleted, 0).
 		Scan(&mainList)
+	for _, one := range mainList {
+		resultList = append(resultList, bean.SimplifyMerchantMember(one))
+	}
 	if err != nil {
 		g.Log().Errorf(ctx, "MerchantMemberList err:%s", err.Error())
-		return mainList, len(mainList)
+		return resultList, len(mainList)
 	}
-	return mainList, len(mainList)
+	return resultList, len(mainList)
 }
