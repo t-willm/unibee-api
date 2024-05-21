86bd4d1503b44f59ba8a6a707b08175c2b406ff0
jack <redcatmiss@126.com>
2024-05-21 12:37:44 +0800

    upgrade to tls api host

--- internal/consumer/init.go
+++ internal/consumer/init.go
@@ -1,6 +1,7 @@
 package consumer
 
 import (
+	_ "unibee/internal/consumer/invoice"
 	_ "unibee/internal/consumer/mock"
 	_ "unibee/internal/consumer/subscription"
 	_ "unibee/internal/consumer/webhook"
