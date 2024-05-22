2d6628ea1a9654a42e5372fb9153328f255a361b
jack <redcatmiss@126.com>
2024-05-22 14:48:33 +0800

    update payment method sync user

--- internal/controller/user/user_profile_change_gateway.go
+++ internal/controller/user/user_profile_change_gateway.go
@@ -2,13 +2,30 @@ package user
 
 import (
 	"context"
+	"strconv"
+	"unibee/internal/consts"
 	_interface "unibee/internal/interface"
 	user2 "unibee/internal/logic/user"
+	"unibee/internal/query"
+	"unibee/utility"
 
 	"unibee/api/user/profile"
 )
 
 func (c *ControllerProfile) ChangeGateway(ctx context.Context, req *profile.ChangeGatewayReq) (res *profile.ChangeGatewayRes, err error) {
+	user := query.GetUserAccountById(ctx, _interface.Context().Get(ctx).User.Id)
+	utility.Assert(user != nil, "user not found")
+	if len(user.GatewayId) > 0 {
+		oldGatewayId, err := strconv.ParseUint(user.GatewayId, 10, 64)
+		if err == nil {
+			gateway := query.GetGatewayById(ctx, oldGatewayId)
+			newGateway := query.GetGatewayById(ctx, req.GatewayId)
+			if oldGatewayId != req.GatewayId {
+				utility.Assert(gateway.GatewayType != consts.GatewayTypeWireTransfer, "Can't change gateway from wire transfer to other, Please contact billing admin")
+				utility.Assert(newGateway.GatewayType != consts.GatewayTypeWireTransfer, "Can't change gateway to wire transfer, Please contact billing admin")
+			}
+		}
+	}
 	user2.UpdateUserDefaultGatewayPaymentMethod(ctx, _interface.Context().Get(ctx).User.Id, req.GatewayId, req.PaymentMethodId)
 	return &profile.ChangeGatewayRes{}, nil
 }
