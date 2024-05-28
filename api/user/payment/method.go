afd00da1aa9d3469b04e4fdeb8c231f633241c60
jack <redcatmiss@126.com>
2024-05-28 15:42:39 +0800

    paypal intergration

--- api/user/payment/method.go
+++ api/user/payment/method.go
@@ -41,7 +41,7 @@ type MethodNewRes struct {
 }
 
 type MethodDeleteReq struct {
-	g.Meta          `path:"/method_delete" tags:"Payment" method:"post" summary:"Delete Payment Method From User"`
+	g.Meta          `path:"/method_delete" tags:"User-Payment-Method" method:"post" summary:"Delete Payment Method From User"`
 	GatewayId       uint64 `json:"gatewayId" dc:"The unique id of gateway"   v:"required" `
 	PaymentMethodId string `json:"paymentMethodId" dc:"The unique id of payment method"  v:"required" `
 }
