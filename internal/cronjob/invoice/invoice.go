8c95b0afa912c54df2f703184384eec425ef252f
jack <redcatmiss@126.com>
2024-05-30 21:36:29 +0800

    update changelly api

--- internal/cronjob/invoice/invoice.go
+++ internal/cronjob/invoice/invoice.go
@@ -52,3 +52,49 @@ func TaskForExpireInvoices(ctx context.Context) {
 		}
 	}
 }
+
+func ExpireUserOneTimeInvoices(ctx context.Context, userId uint64) {
+	if userId <= 0 {
+		return
+	}
+	var list []*entity.Invoice
+	err := dao.Invoice.Ctx(ctx).
+		Where(dao.Invoice.Columns().UserId, userId).
+		Where(dao.Invoice.Columns().BizType, consts.BizTypeOneTime).
+		Where(dao.Invoice.Columns().Status, consts.InvoiceStatusProcessing).
+		Where(dao.Invoice.Columns().IsDeleted, 0).
+		OrderAsc(dao.Invoice.Columns().FinishTime).
+		Scan(&list)
+	if err != nil {
+		g.Log().Errorf(ctx, "ExpireUserOneTimeInvoices error:%s", err.Error())
+		return
+	}
+	for _, one := range list {
+		key := fmt.Sprintf("TaskForExpireInvoices-%v", one.Id)
+		if utility.TryLock(ctx, key, 60) {
+			g.Log().Print(ctx, "ExpireUserOneTimeInvoices GetLock 60s", key)
+			defer func() {
+				utility.ReleaseLock(ctx, key)
+				g.Log().Print(ctx, "ExpireUserOneTimeInvoices ReleaseLock", key)
+			}()
+			if one.FinishTime == 0 {
+				_, err = dao.Invoice.Ctx(ctx).Data(g.Map{
+					dao.Invoice.Columns().FinishTime: one.GmtModify.Timestamp(),
+					dao.Invoice.Columns().GmtModify:  gtime.Now(),
+				}).Where(dao.Invoice.Columns().Id, one.Id).OmitNil().Update()
+				if err != nil {
+					g.Log().Errorf(ctx, "ExpireUserOneTimeInvoices Update FinishTime error:", err.Error())
+				}
+			} else if one.FinishTime+(one.DayUtilDue*86400) < gtime.Now().Timestamp() {
+				//Invoice Expire
+				err = service.ProcessingInvoiceFailure(ctx, one.InvoiceId)
+				if err != nil {
+					g.Log().Errorf(ctx, "ExpireUserOneTimeInvoices Failure invoice error:", err.Error())
+				}
+			}
+		} else {
+			g.Log().Errorf(ctx, "ExpireUserOneTimeInvoices GetLock Failure", key)
+			return
+		}
+	}
+}
--- internal/logic/subscription/billingcycle/testclock/testclock.go
+++ internal/logic/subscription/billingcycle/testclock/testclock.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 	"time"
 	"unibee/internal/cmd/config"
+	"unibee/internal/cronjob/invoice"
 	dao "unibee/internal/dao/oversea_pay"
 	"unibee/internal/logic/subscription/billingcycle/cycle"
 	"unibee/internal/query"
@@ -26,6 +27,11 @@ func WalkSubscriptionToTestClock(ctx context.Context, subId string, newTestClock
 	utility.Assert(newTestClock > 0, "Invalid TestClock")
 	sub := query.GetSubscriptionBySubscriptionId(ctx, subId)
 	utility.Assert(sub != nil, "Subscription Not Found")
+	go func() {
+		if sub.UserId > 0 {
+			invoice.ExpireUserOneTimeInvoices(ctx, sub.UserId)
+		}
+	}()
 	//utility.Assert(sub.Status != consts.SubStatusExpired && sub.Status != consts.SubStatusCancelled, "Subscription Has Cancel or Expire")
 	utility.Assert(sub.TestClock < newTestClock, "The Subscription Has Walk To The TestClock Exceed The New One")
 
