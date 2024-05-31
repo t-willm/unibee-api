5fd55b95e4bc22e4bafc3be43ac874332e04eb4e
jack <redcatmiss@126.com>
2024-05-31 21:06:36 +0800

    update changelly api

--- internal/controller/merchant/merchant_vat_country_list.go
+++ internal/controller/merchant/merchant_vat_country_list.go
@@ -9,9 +9,9 @@ import (
 )
 
 func (c *ControllerVat) CountryList(ctx context.Context, req *vat.CountryListReq) (res *vat.CountryListRes, err error) {
-	list, err := vat_gateway.MerchantCountryRateList(ctx, _interface.GetMerchantId(ctx))
-	if err != nil {
-		return nil, err
-	}
+	list, _ := vat_gateway.MerchantCountryRateList(ctx, _interface.GetMerchantId(ctx))
+	//if err != nil {
+	//	return nil, err
+	//}
 	return &vat.CountryListRes{VatCountryList: list}, nil
 }
