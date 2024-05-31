2c4c640d449e61e7620a6289475dacc7d5c3efcb
jack <redcatmiss@126.com>
2024-06-01 00:14:12 +0800

    update changelly api

--- internal/logic/gateway/api/stripe.go
+++ internal/logic/gateway/api/stripe.go
@@ -572,20 +572,46 @@ func (s Stripe) GatewayNewPayment(ctx context.Context, createPayContext *gateway
 			return nil, err
 		}
 
+		var containNegative = false
 		for _, line := range createPayContext.Invoice.Lines {
-			var description = line.Description
-			if len(description) == 0 {
-				if line.Plan != nil {
-					description = line.Plan.PlanName
-				} else {
-					description = "Default Product"
+			if line.Amount <= 0 {
+				containNegative = true
+			}
+		}
+		if !containNegative {
+			for _, line := range createPayContext.Invoice.Lines {
+				var description = line.Description
+				if len(description) == 0 {
+					if line.Plan != nil {
+						description = line.Plan.PlanName
+					} else {
+						description = "Default Product"
+					}
 				}
+				ItemParams := &stripe.InvoiceItemParams{
+					Invoice:     stripe.String(result.ID),
+					Currency:    stripe.String(strings.ToLower(createPayContext.Invoice.Currency)),
+					Amount:      stripe.Int64(line.Amount),
+					Description: stripe.String(description),
+					Customer:    stripe.String(gatewayUser.GatewayUserId)}
+				_, err = invoiceitem.New(ItemParams)
+				if err != nil {
+					return nil, err
+				}
+			}
+		} else {
+			var productName = createPayContext.Invoice.ProductName
+			if len(productName) == 0 {
+				productName = createPayContext.Invoice.InvoiceName
+			}
+			if len(productName) == 0 {
+				productName = "DefaultProduct"
 			}
 			ItemParams := &stripe.InvoiceItemParams{
 				Invoice:     stripe.String(result.ID),
 				Currency:    stripe.String(strings.ToLower(createPayContext.Invoice.Currency)),
-				Amount:      stripe.Int64(line.Amount),
-				Description: stripe.String(description),
+				Amount:      stripe.Int64(createPayContext.Invoice.TotalAmount),
+				Description: stripe.String(fmt.Sprintf("%s", productName)),
 				Customer:    stripe.String(gatewayUser.GatewayUserId)}
 			_, err = invoiceitem.New(ItemParams)
 			if err != nil {
