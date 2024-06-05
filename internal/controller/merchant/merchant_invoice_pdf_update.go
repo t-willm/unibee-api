c18d9eaa08f5b44cfaaf504656bf7a4df139301e
jack <redcatmiss@126.com>
2024-06-06 06:32:54 +0800

    heartBeat memorytool

--- internal/controller/merchant/merchant_invoice_pdf_update.go
+++ internal/controller/merchant/merchant_invoice_pdf_update.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gtime"
 	dao "unibee/internal/dao/oversea_pay"
+	_interface "unibee/internal/interface"
 	"unibee/internal/query"
 	"unibee/utility"
 
@@ -14,6 +15,7 @@ import (
 func (c *ControllerInvoice) PdfUpdate(ctx context.Context, req *invoice.PdfUpdateReq) (res *invoice.PdfUpdateRes, err error) {
 	one := query.GetInvoiceByInvoiceId(ctx, req.InvoiceId)
 	if one != nil && len(one.SendPdf) == 0 && req.SendPdf != nil && len(*req.SendPdf) > 0 {
+		utility.Assert(one.MerchantId == _interface.GetMerchantId(ctx), "no permission")
 		_, err = dao.Invoice.Ctx(ctx).Data(g.Map{
 			dao.Invoice.Columns().SendPdf:   *req.SendPdf,
 			dao.Invoice.Columns().GmtModify: gtime.Now(),
--- internal/controller/merchant/merchant_invoice_send_email.go
+++ internal/controller/merchant/merchant_invoice_send_email.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gogf/gf/v2/os/gtime"
 	"unibee/api/merchant/invoice"
 	dao "unibee/internal/dao/oversea_pay"
+	_interface "unibee/internal/interface"
 	"unibee/internal/logic/invoice/handler"
 	"unibee/internal/query"
 	"unibee/utility"
@@ -19,6 +20,7 @@ func (c *ControllerInvoice) SendEmail(ctx context.Context, req *invoice.SendEmai
 	}
 	one := query.GetInvoiceByInvoiceId(ctx, req.InvoiceId)
 	if one != nil && len(one.SendPdf) == 0 && req.SendPdf != nil && len(*req.SendPdf) > 0 {
+		utility.Assert(one.MerchantId == _interface.GetMerchantId(ctx), "no permission")
 		_, err = dao.Invoice.Ctx(ctx).Data(g.Map{
 			dao.Invoice.Columns().SendPdf:   *req.SendPdf,
 			dao.Invoice.Columns().GmtModify: gtime.Now(),
