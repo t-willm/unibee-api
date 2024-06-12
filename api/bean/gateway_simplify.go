15f170245fcf2757e76d8b80129d4019f859cb82
jack <redcatmiss@126.com>
2024-06-12 17:27:56 +0800

    minor updates

--- api/bean/gateway_simplify.go
+++ api/bean/gateway_simplify.go
@@ -4,11 +4,13 @@ import (
 	"unibee/internal/consts"
 	gateway2 "unibee/internal/logic/gateway"
 	entity "unibee/internal/model/entity/oversea_pay"
+	"unicode"
 )
 
 type GatewaySimplify struct {
 	Id                 uint64          `json:"gatewayId"`
 	GatewayName        string          `json:"gatewayName"`
+	DisplayName        string          `json:"displayName"`
 	GatewayLogo        string          `json:"gatewayLogo"`
 	GatewayKey         string          `json:"gatewayKey"            description:""`                                                        //
 	GatewayType        int64           `json:"gatewayType"           description:"gateway type，1-Bank Card ｜ 2-Crypto | 3 - Wire Transfer"` // gateway type，1-Default｜ 2-Crypto
@@ -40,10 +42,12 @@ func SimplifyGateway(one *entity.MerchantGateway) *GatewaySimplify {
 	if one.GatewayType != consts.GatewayTypeWireTransfer {
 		webhookEndpointUrl = gateway2.GetPaymentWebhookEntranceUrl(one.Id)
 	}
+
 	return &GatewaySimplify{
 		Id:                 one.Id,
 		GatewayLogo:        one.Logo,
 		GatewayName:        one.GatewayName,
+		DisplayName:        toUpperFirst(one.GatewayName),
 		GatewayType:        one.GatewayType,
 		CountryConfig:      countryConfig,
 		CreateTime:         one.CreateTime,
@@ -54,6 +58,15 @@ func SimplifyGateway(one *entity.MerchantGateway) *GatewaySimplify {
 		GatewayKey:         one.GatewayKey,
 		WebhookSecret:      one.WebhookSecret,
 	}
+}
+
+func toUpperFirst(s string) string {
+	if s == "" {
+		return s
+	}
+	runes := []rune(s)
+	runes[0] = unicode.ToUpper(runes[0])
+	return string(runes)
 }
 
 func SimplifyGatewayList(ones []*entity.MerchantGateway) (list []*GatewaySimplify) {
