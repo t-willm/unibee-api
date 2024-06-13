0eff83d1ee4970596d67451024d43ab67a9797ef
jack <redcatmiss@126.com>
2024-06-13 15:34:53 +0800

    minor updates

--- internal/controller/user/user_vat_number_validate.go
+++ internal/controller/user/user_vat_number_validate.go
@@ -2,15 +2,10 @@ package user
 
 import (
 	"context"
-	_interface "unibee/internal/interface"
-	"unibee/internal/logic/subscription/service"
-	"unibee/utility"
-
 	"unibee/api/user/vat"
+	"unibee/internal/logic/subscription/service"
 )
 
 func (c *ControllerVat) NumberValidate(ctx context.Context, req *vat.NumberValidateReq) (res *vat.NumberValidateRes, err error) {
-
-	utility.Assert(_interface.Context().Get(ctx).User != nil, "auth failure,not login")
 	return service.VatNumberValidate(ctx, req)
 }
