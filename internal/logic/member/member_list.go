0c97bb9d03cd4916f1e553efa1949d1c46db0f6e
jack <redcatmiss@126.com>
2024-06-21 10:56:37 +0800

    minor updates

--- internal/logic/member/member_list.go
+++ internal/logic/member/member_list.go
@@ -15,13 +15,14 @@ func MerchantMemberList(ctx context.Context, merchantId uint64, page int, count 
 	if page < 0 {
 		page = 0
 	}
+	var total = 0
 	var resultList = make([]*detail.MerchantMemberDetail, 0)
 	var mainList = make([]*entity.MerchantMember, 0)
 	err := dao.MerchantMember.Ctx(ctx).
 		Where(dao.MerchantMember.Columns().MerchantId, merchantId).
 		Where(dao.MerchantMember.Columns().IsDeleted, 0).
 		Limit(page*count, count).
-		Scan(&mainList)
+		ScanAndCount(&mainList, &total, true)
 	if err != nil {
 		g.Log().Errorf(ctx, "MerchantMemberList err:%s", err.Error())
 		return resultList, len(resultList)
@@ -29,7 +30,7 @@ func MerchantMemberList(ctx context.Context, merchantId uint64, page int, count 
 	for _, one := range mainList {
 		resultList = append(resultList, detail.ConvertMemberToDetail(ctx, one))
 	}
-	return resultList, len(resultList)
+	return resultList, total
 }
 
 func MerchantMemberTotalList(ctx context.Context, merchantId uint64) ([]*detail.MerchantMemberDetail, int) {
