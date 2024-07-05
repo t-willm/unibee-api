90d10cfe763fd3a3e21159cb4dc0cbf0660613e2
jack <redcatmiss@126.com>
2024-07-05 11:37:10 +0800

    minor updates

--- internal/logic/batch/_import/subscription/active_subscription.go
+++ internal/logic/batch/_import/subscription/active_subscription.go
@@ -3,7 +3,9 @@ package subscription
 import (
 	"context"
 	"fmt"
+	"github.com/gogf/gf/v2/errors/gerror"
 	entity "unibee/internal/model/entity/oversea_pay"
+	"unibee/internal/query"
 )
 
 type TaskActiveSubscriptionImport struct {
@@ -26,19 +28,55 @@ func (t TaskActiveSubscriptionImport) ImportRow(ctx context.Context, task *entit
 		Amount:                 fmt.Sprintf("%s", row["Amount"]),
 		Currency:               fmt.Sprintf("%s", row["Currency"]),
 		Quantity:               fmt.Sprintf("%s", row["Quantity"]),
-		Gateway:                fmt.Sprintf("%s", row["Gateway"]),
-		Status:                 fmt.Sprintf("%s", row["Status"]),
+		Gateway:                fmt.Sprintf("%s", row["Gateway(stripe|paypal|wire_transfer|changelly)"]),
 		CurrentPeriodStart:     fmt.Sprintf("%s", row["CurrentPeriodStart"]),
 		CurrentPeriodEnd:       fmt.Sprintf("%s", row["CurrentPeriodEnd"]),
 		BillingCycleAnchor:     fmt.Sprintf("%s", row["BillingCycleAnchor"]),
-		DunningTime:            fmt.Sprintf("%s", row["DunningTime"]),
-		TrialEnd:               fmt.Sprintf("%s", row["TrialEnd"]),
 		FirstPaidTime:          fmt.Sprintf("%s", row["FirstPaidTime"]),
 		CreateTime:             fmt.Sprintf("%s", row["CreateTime"]),
 		StripeUserId:           fmt.Sprintf("%s", row["StripeUserId(Auto-Charge Required)"]),
 		StripePaymentMethod:    fmt.Sprintf("%s", row["StripePaymentMethod(Auto-Charge Required)"]),
 		PaypalVaultId:          fmt.Sprintf("%s", row["PaypalVaultId(Auto-Charge Required)"]),
 	}
+	if len(target.ExternalUserId) == 0 {
+		return target, gerror.New("Error, ExternalUserId is blank")
+	}
+	if len(target.ExternalPlanId) == 0 {
+		return target, gerror.New("Error, ExternalPlanId is blank")
+	}
+	if len(target.Amount) == 0 {
+		return target, gerror.New("Error, Amount is blank")
+	}
+	if len(target.Currency) == 0 {
+		return target, gerror.New("Error, Currency is blank")
+	}
+	if len(target.Gateway) == 0 {
+		return target, gerror.New("Error, Gateway is blank")
+	}
+	if len(target.CurrentPeriodStart) == 0 {
+		return target, gerror.New("Error, CurrentPeriodStart is blank")
+	}
+	if len(target.CurrentPeriodEnd) == 0 {
+		return target, gerror.New("Error, CurrentPeriodEnd is blank")
+	}
+	if len(target.BillingCycleAnchor) == 0 {
+		return target, gerror.New("Error, BillingCycleAnchor is blank")
+	}
+	if len(target.FirstPaidTime) == 0 {
+		return target, gerror.New("Error, FirstPaidTime is blank")
+	}
+	if len(target.CreateTime) == 0 {
+		return target, gerror.New("Error, CreateTime is blank")
+	}
+	plan := query.GetPlanByExternalPlanId(ctx, task.MerchantId, target.ExternalPlanId)
+	if plan == nil {
+		return target, gerror.New("Error, can't find plan by ExternalPlanId")
+	}
+	user := query.GetUserAccountByExternalUserId(ctx, task.MerchantId, target.ExternalUserId)
+	if user == nil {
+		return target, gerror.New("Error, can't find user by ExternalUserId")
+	}
+
 	return target, err
 }
 
@@ -49,16 +87,14 @@ type ImportActiveSubscriptionEntity struct {
 	Amount                 string `json:"Amount"             `
 	Currency               string `json:"Currency"           `
 	Quantity               string `json:"Quantity"           `
-	Gateway                string `json:"Gateway"            `
-	Status                 string `json:"Status"             `
+	Gateway                string `json:"Gateway(stripe|paypal|wire_transfer|changelly)"            `
 	CurrentPeriodStart     string `json:"CurrentPeriodStart" `
 	CurrentPeriodEnd       string `json:"CurrentPeriodEnd"   `
 	BillingCycleAnchor     string `json:"BillingCycleAnchor" `
-	DunningTime            string `json:"DunningTime"        `
-	TrialEnd               string `json:"TrialEnd"           `
 	FirstPaidTime          string `json:"FirstPaidTime"      `
 	CreateTime             string `json:"CreateTime"         `
 	StripeUserId           string `json:"StripeUserId(Auto-Charge Required)"             `
 	StripePaymentMethod    string `json:"StripePaymentMethod(Auto-Charge Required)"      `
 	PaypalVaultId          string `json:"PaypalVaultId(Auto-Charge Required)"      `
+	Features               string `json:"Features"         `
 }
--- internal/logic/batch/_import/user/user.go
+++ internal/logic/batch/_import/user/user.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gtime"
+	dao "unibee/internal/dao/oversea_pay"
 	"unibee/internal/logic/auth"
 	entity "unibee/internal/model/entity/oversea_pay"
 	"unibee/internal/query"
@@ -23,26 +26,44 @@ func (t TaskUserImport) TemplateHeader() interface{} {
 func (t TaskUserImport) ImportRow(ctx context.Context, task *entity.MerchantBatchTask, row map[string]string) (interface{}, error) {
 	var err error
 	target := &ImportUserEntity{
-		FirstName: fmt.Sprintf("%s", row["FirstName"]),
-		LastName:  fmt.Sprintf("%s", row["LastName"]),
-		Email:     fmt.Sprintf("%s", row["Email"]),
-		Address:   fmt.Sprintf("%s", row["Address"]),
+		Email:          fmt.Sprintf("%s", row["Email"]),
+		ExternalUserId: fmt.Sprintf("%s", row["ExternalUserId"]),
+		FirstName:      fmt.Sprintf("%s", row["FirstName"]),
+		LastName:       fmt.Sprintf("%s", row["LastName"]),
+		Address:        fmt.Sprintf("%s", row["Address"]),
 		//VATNumber:      fmt.Sprintf("%s", row["VATNumber"]),
 		//CountryCode:    fmt.Sprintf("%s", row["CountryCode"]),
-		ExternalUserId: fmt.Sprintf("%s", row["ExternalUserId"]),
-		Phone:          fmt.Sprintf("%s", row["Phone"]),
+		Phone: fmt.Sprintf("%s", row["Phone"]),
 		//TaxPercentage:  fmt.Sprintf("%s", row["TaxPercentage"]),
 		//Type:           fmt.Sprintf("%s", row["Type"]),
 	}
+	if len(target.Email) == 0 {
+		return target, gerror.New("Error, Email is blank")
+	}
 	one := query.GetUserAccountByEmail(ctx, task.MerchantId, target.Email)
 	if one != nil {
-		return target, gerror.New("Skip, same email user exist")
+		//enter update process
+		if len(target.ExternalUserId) > 0 {
+			otherOne := query.GetUserAccountByExternalUserId(ctx, task.MerchantId, target.ExternalUserId)
+			if otherOne != nil && one.Id != otherOne.Id {
+				return target, gerror.New("Error, same ExternalUserId user exist")
+			}
+		}
+		_, err = dao.UserAccount.Ctx(ctx).Data(g.Map{
+			dao.UserAccount.Columns().ExternalUserId: target.ExternalUserId,
+			dao.UserAccount.Columns().FirstName:      target.FirstName,
+			dao.UserAccount.Columns().LastName:       target.LastName,
+			dao.UserAccount.Columns().Address:        target.Address,
+			dao.UserAccount.Columns().Phone:          target.Phone,
+			dao.UserAccount.Columns().GmtModify:      gtime.Now(),
+		}).Where(dao.UserAccount.Columns().Id, one.Id).OmitEmpty().Update()
+		return target, err
 	}
 	if len(target.ExternalUserId) > 0 {
 		one = query.GetUserAccountByExternalUserId(ctx, task.MerchantId, target.ExternalUserId)
 	}
 	if one != nil {
-		return target, gerror.New("Skip, same ExternalUserId user exist")
+		return target, gerror.New("Error, same ExternalUserId user exist")
 	}
 	_, err = auth.CreateUser(ctx, &auth.NewReq{
 		ExternalUserId: target.ExternalUserId,
@@ -51,22 +72,21 @@ func (t TaskUserImport) ImportRow(ctx context.Context, task *entity.MerchantBatc
 		LastName:       target.LastName,
 		Address:        target.Address,
 		Phone:          target.Phone,
-		//CountryCode:    target.CountryCode,
-		Custom:     "Import",
-		MerchantId: task.MerchantId,
+		Custom:         "Import",
+		MerchantId:     task.MerchantId,
 	})
 	return target, err
 }
 
 type ImportUserEntity struct {
-	FirstName string `json:"FirstName"          `
-	LastName  string `json:"LastName"           `
-	Email     string `json:"Email"              `
-	Address   string `json:"Address"            `
-	Phone     string `json:"Phone"            `
+	Email          string `json:"Email"              `
+	ExternalUserId string `json:"ExternalUserId"     `
+	FirstName      string `json:"FirstName"          `
+	LastName       string `json:"LastName"           `
+	Address        string `json:"Address"            `
+	Phone          string `json:"Phone"            `
 	//VATNumber      string `json:"VATNumber"          `
 	//CountryCode    string `json:"CountryCode"        `
-	ExternalUserId string `json:"ExternalUserId"     `
 	//TaxPercentage  string `json:"TaxPercentage"      `
 	//Type           string `json:"Type"               `
 }
