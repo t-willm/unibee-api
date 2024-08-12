3f31ef91f12ca70d582ff9b09b7ea5224ca87caa
jack <redcatmiss@126.com>
2024-08-12 23:10:42 +0800

    platform api

--- internal/controller/merchant/merchant_auth_password_forget_otp.go
+++ internal/controller/merchant/merchant_auth_password_forget_otp.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 	"unibee/internal/cmd/i18n"
 	"unibee/internal/logic/email"
+	"unibee/internal/logic/platform"
 	"unibee/internal/query"
 	"unibee/utility"
 
@@ -28,11 +29,25 @@ func (c *ControllerAuth) PasswordForgetOtp(ctx context.Context, req *auth.Passwo
 	merchantMember := query.GetMerchantMemberByEmail(ctx, req.Email)
 	utility.Assert(merchantMember != nil, "merchant member not found")
 	utility.Assert(merchantMember.Status != 2, "Your account has been suspended. Please contact billing admin for further assistance.")
-	err = email.SendTemplateEmail(ctx, merchantMember.MerchantId, req.Email, "", email.TemplateUserOTPLogin, "", &email.TemplateVariable{
-		UserName:         merchantMember.FirstName + " " + merchantMember.LastName,
-		CodeExpireMinute: "3",
-		Code:             verificationCode,
-	})
+	_, emailGatewayKey := email.GetDefaultMerchantEmailConfig(ctx, merchantMember.MerchantId)
+	if len(emailGatewayKey) > 0 {
+		err = email.SendTemplateEmail(ctx, merchantMember.MerchantId, req.Email, "", email.TemplateUserOTPLogin, "", &email.TemplateVariable{
+			UserName:         merchantMember.FirstName + " " + merchantMember.LastName,
+			CodeExpireMinute: "3",
+			Code:             verificationCode,
+		})
+	} else {
+		merchant := query.GetMerchantById(ctx, merchantMember.MerchantId)
+		utility.Assert(merchant != nil, "merchant not found")
+		// send to UniBee platform api
+		err = platform.SentPlatformMerchantOTP(map[string]string{
+			"ownerEmail":  merchant.Email,
+			"memberEmail": req.Email,
+			"firstName":   merchantMember.FirstName,
+			"lastName":    merchantMember.LastName,
+			"code":        verificationCode,
+		})
+	}
 	utility.AssertError(err, "Server Error")
 	return &auth.PasswordForgetOtpRes{}, nil
 }
