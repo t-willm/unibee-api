f71675f91d39db9bfc9868fd32f6a0bab91f03af
jack <redcatmiss@126.com>
2024-08-31 07:42:41 +0800

    checkout vat rules

--- utility/download.go
+++ utility/download.go
@@ -9,48 +9,42 @@ import (
 )
 
 func DownloadFile(url string) string {
-	// 获取图片文件名
 	fileName := filepath.Base(url)
 
 	currentDir, err := os.Getwd()
-	// 构建本地文件路径
 	localFilePath := filepath.Join(currentDir, fileName)
-	// 创建文件
 	file, err := os.Create(fileName)
 	if err != nil {
-		fmt.Println("Error creating file:", err)
+		fmt.Printf("Error creating file:%s\n", err.Error())
 		return ""
 	}
 	defer func(file *os.File) {
 		err := file.Close()
 		if err != nil {
-
+			fmt.Printf("Error creating file:%s\n", err.Error())
 		}
 	}(file)
 
-	// 发起 HTTP 请求获取图片
 	response, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Error downloading image:", err)
+		fmt.Printf("Error downloading file:%s\n", err.Error())
 		return ""
 	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-
+			fmt.Printf("Error creating file:%s\n", err.Error())
 		}
 	}(response.Body)
 
-	// 检查 HTTP 状态码
 	if response.StatusCode != http.StatusOK {
-		fmt.Println("Error:", response.Status)
+		fmt.Printf("Error:%s\n", response.Status)
 		return ""
 	}
 
-	// 将内容写入文件
 	_, err = io.Copy(file, response.Body)
 	if err != nil {
-		fmt.Println("Error writing to file:", err)
+		fmt.Printf("Error writing to file:%s\n", err.Error())
 		return ""
 	}
 	fmt.Println("File downloaded successfully:", localFilePath)
