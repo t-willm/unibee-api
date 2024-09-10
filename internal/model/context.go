f8d5dca1f404d153c14dae7d1ea20e6290035f24
ds <fuzehua@syoo.cn>
2024-09-11 05:31:45 +0800

    bugfix

--- internal/model/context.go
+++ internal/model/context.go
@@ -1,11 +1,9 @@
 package model
 
 import (
-	"github.com/golang-jwt/jwt/v5"
-	entity "unibee/internal/model/entity/default"
-
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type TokenType string
@@ -20,6 +18,20 @@ type TokenClaims struct {
 	jwt.RegisteredClaims
 }
 
+type OpenApiConfig struct {
+	Id                      uint64 `json:"id"                      description:""`                         //
+	Qps                     int    `json:"qps"                     description:"total qps control"`        // total qps control
+	MerchantId              uint64 `json:"merchantId"              description:"merchant id"`              // merchant id
+	Hmac                    string `json:"hmac"                    description:"webhook hmac key"`         // webhook hmac key
+	Callback                string `json:"callback"                description:"callback url"`             // callback url
+	ApiKey                  string `json:"apiKey"                  description:"api key"`                  // api key
+	Token                   string `json:"token"                   description:"api token"`                // api token
+	IsDeleted               int    `json:"isDeleted"               description:"0-UnDeleted，1-Deleted"`    // 0-UnDeleted，1-Deleted
+	ValidIps                string `json:"validIps"                description:""`                         //
+	GatewayCallbackResponse string `json:"gatewayCallbackResponse" description:"callback return response"` // callback return response
+	CompanyId               int64  `json:"companyId"               description:"company id"`               // company id
+}
+
 type Context struct {
 	Session        *ghttp.Session
 	MerchantId     uint64
@@ -27,7 +39,7 @@ type Context struct {
 	MerchantMember *ContextMerchantMember
 	RequestId      string
 	Data           g.Map
-	OpenApiConfig  *entity.OpenApiConfig
+	OpenApiConfig  *OpenApiConfig
 	OpenApiKey     string
 	IsOpenApiCall  bool
 	Language       string
